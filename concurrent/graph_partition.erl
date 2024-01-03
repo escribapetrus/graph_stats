819eabfcbf04fa6cd83a3f7a2e6d8cca82dd5591
pschreiber <schreiber.mmb@gmail.com>
2024-01-02 22:26:41 -0300

    add fetch degree by node id from supervisor

--- concurrent/graph_partition.erl
+++ concurrent/graph_partition.erl
@@ -2,10 +2,14 @@
 -behaviour(gen_server).
 -export([start_link/1]).
 -export([init/1, handle_call/3, handle_cast/2]).
--export([color_count_and_degrees/1, most_influential_nodes/1]).
+-export([
+         color_count_and_degrees/1, 
+         most_influential_nodes/1,
+         degree/2]).
 -export([
          test_color_count_and_degrees/0,
-         test_most_influential_nodes/0]).
+         test_most_influential_nodes/0,
+         test_degree/0]).
 
 -record(node, {id, color, edges = []}).
 
@@ -18,15 +22,51 @@ color_count_and_degrees(Pid) ->
 most_influential_nodes(Pid) ->
     gen_server:cast(Pid, most_influential_nodes).
 
+degree(Pid, NodeId) ->
+    gen_server:call(Pid, {degree, NodeId}).
+
 init(Nodes) ->
     {ok, Nodes}.
+
+handle_call({degree, NodeId}, _From, Nodes) ->
+    case lists:search(fun(N) -> N#node.id == NodeId end, Nodes) of
+        false ->
+            {reply, 0, Nodes};
+        {value, Value} -> 
+            {reply, length(Value#node.edges), Nodes}
+    end;
 
 handle_call(Message, _From, State) ->
     {reply, Message, State}.
 
-handle_cast(most_influential_nodes, State) ->
-    io:format("calculating most influential nodes.~n", []),
-    {noreply, State};
+handle_cast(most_influential_nodes, Nodes) ->
+    %% - Get the degree of each internal node of Ap .
+    %% - Find the list of external nodes connected to Ap . 
+    %%   (Any node in the edge list that is not in the node list is an
+    %%   external node of Ap )
+    %% - For each external node n, message all other actors 
+    %%   requesting the degree of node n. (An actor should return
+    %%   0 if node n does not exist in their local partition).
+    %% - Find the largest degree node(s) from among the 
+    %%   internal and external nodes of actor Ap . If there are multiple
+    %%   nodes of the same max degree, return all such nodes.
+    NodeIds = [N#node.id || N <- Nodes],    
+    Edges = lists:flatten([N#node.edges || N <- Nodes]),
+    ExternalNodes = lists:filter(fun(X) -> not lists:member(X, NodeIds) end, Edges),
+    Res = lists:foldl(fun(Elem, [{_NodeId, Degree}|_] = Acc) -> 
+                        case graph_supervisor:fetch_degree(Elem) of
+                            {NodeId, Value} when Value > Degree ->
+                                [{NodeId, Value}];
+                            {NodeId, Value} when Value == Degree ->
+                                [{NodeId, Value}| Acc];
+                            _ -> Acc
+                                     
+                            end
+                end,
+                [{0, 0}],
+                NodeIds ++ ExternalNodes),
+    erlang:display(Res),
+    {noreply, Nodes};
 
 handle_cast(color_count_and_degrees, Nodes) ->
     GroupByColor = fun(#node{color = Color}) -> Color end,
@@ -66,26 +106,30 @@ test_most_influential_nodes() ->
     end,
     [Pid | _] = lists:map(StartPartition, test_data_nodes()),
     most_influential_nodes(Pid),
-    erlang:display(sys:get_state(Pid)).
+    ok.
+
+test_degree() -> 
+    [Pid | _] = lists:map(fun(Nodes) ->
+        {ok, Pid} = graph_partition:start_link(Nodes), 
+        Pid 
+    end, test_data_nodes()),
+    true = 4 == degree(Pid, 0),
+    true = 3 == degree(Pid, 3),
+    true = 0 == degree(Pid, 9),
+    true = 0 == degree(Pid, 12).
 
 test_data_nodes() ->
-    lists:flatten([
-     [
-      {node,"0","blue",["1","2","3","9"]},
-      {node,"1","green",["2","3","4","0"]},
-      {node,"2","blue",["3","0","1"]},
-      {node,"3","red",["0","1","2"]}
-     ],
-     [
-      {node,"4","green",["5","6","7","1"]},
-      {node,"5","red",["6","7","8","4"]},
-      {node,"6","green",["7","4","5"]},
-      {node,"7","green",["4","5","6"]}],
-     [
-      {node,"8","red",["9","10","11","5"]},
-      {node,"9","blue",["10","11","0","8"]},
-      {node,"10","blue",["11","8","9"]},
-      {node,"11","red",["8","9","10"]}
-     ]]).
+    [[{node,0,"blue",[1,2,3,9]},
+      {node,1,"green",[2,3,4,0]},
+      {node,2,"blue",[3,0,1]},
+      {node,3,"red",[0,1,2]}],
+     [{node,4,"green",[5,6,7,1]},
+      {node,5,"red",[6,7,8,4]},
+      {node,6,"green",[7,4,5]},
+      {node,7,"green",[4,5,6]}],
+     [{node,8,"red",[9,10,11,5]},
+      {node,9,"blue",[10,11,0,8]},
+      {node,10,"blue",[11,8,9]},
+      {node,11,"red",[8,9,10]}]].
 
 
--- concurrent/graph_supervisor.erl
+++ concurrent/graph_supervisor.erl
@@ -1,8 +1,10 @@
 -module(graph_supervisor).
 -behaviour(supervisor).
--export([start_link/2, add_partition/2, calculate_most_influential_nodes/0]).
+-export([start_link/2, add_partition/2, calculate_most_influential_nodes/0,
+        fetch_degree/1]).
 -export([init/1]).
--export([test_calculate_most_influential_nodes/0]).
+-export([test_calculate_most_influential_nodes/0,
+         test_fetch_degree/0]).
 
 start_link(ResultAPath, ResultBPath) ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, [ResultAPath, ResultBPath]).
@@ -18,9 +20,21 @@ add_partition(Id, Nodes) ->
 calculate_most_influential_nodes() ->
     Children = supervisor:which_children(?MODULE),
     SendMessage = fun({_Id, Pid, _, _}) -> 
-                          graph_partition:calculate_most_influential_nodes(Pid) end,
+                          graph_partition:most_influential_nodes(Pid) end,
     lists:foreach(SendMessage, Children).
 
+fetch_degree(NodeId) ->
+    Children = supervisor:which_children(?MODULE),
+    fetch_degree(NodeId, Children).
+
+fetch_degree(NodeId, []) -> {NodeId, 0};
+fetch_degree(NodeId, [{"partition" ++ _, Pid, _, _}|Rest]) ->
+    case graph_partition:degree(Pid, NodeId) of
+        0 -> fetch_degree(NodeId, Rest);
+        Value -> {NodeId, Value}
+    end;
+fetch_degree(NodeId, [_|Rest]) -> fetch_degree(NodeId, Rest).
+    
 
 init([ResultAPath, ResultBPath]) ->
     SupFlags = #{strategy => one_for_one,
@@ -39,25 +53,31 @@ init([ResultAPath, ResultBPath]) ->
 %%%%%%%%%%%% TESTS
 
 test_calculate_most_influential_nodes() ->
-    TestData = [
-     {"partition 0", [
-      {node,"0","blue",["1","2","3","9"]},
-      {node,"1","green",["2","3","4","0"]},
-      {node,"2","blue",["3","0","1"]},
-      {node,"3","red",["0","1","2"]}]},
-     {"partition 1", [
-      {node,"4","green",["5","6","7","1"]},
-      {node,"5","red",["6","7","8","4"]},
-      {node,"6","green",["7","4","5"]},
-      {node,"7","green",["4","5","6"]}]},
-     {"partition 2", [
-      {node,"8","red",["9","10","11","5"]},
-      {node,"9","blue",["10","11","0","8"]},
-      {node,"10","blue",["11","8","9"]},
-      {node,"11","red",["8","9","10"]}]}],
-    AddPartition = fun({Id, Nodes}) -> add_partition(Id, Nodes) end,
     graph_supervisor:start_link("../test_graph_supervisor_a.txt", "../test_graph_supervisor_b"),
-    lists:foreach(AddPartition, TestData),
+    lists:foreach(fun({Id, Nodes}) -> add_partition(Id, Nodes) end, test_data_nodes()),
     calculate_most_influential_nodes().
 
+test_fetch_degree() -> 
+    graph_supervisor:start_link("../test_graph_supervisor_a.txt", "../test_graph_supervisor_b"),
+    lists:foreach(fun({Id, Nodes}) -> add_partition(Id, Nodes) end, test_data_nodes()),
+    true = {9, 4} == fetch_degree(9),
+    true = {1, 4} == fetch_degree(1),
+    true = {12, 0} == fetch_degree(12).
 
+
+test_data_nodes() -> 
+    [{"partition 0", [
+      {node,0,"blue",[1,2,3,9]},
+      {node,1,"green",[2,3,4,0]},
+      {node,2,"blue",[3,0,1]},
+      {node,3,"red",[0,1,2]}]},
+     {"partition 1", [
+      {node,4,"green",[5,6,7,1]},
+      {node,5,"red",[6,7,8,4]},
+      {node,6,"green",[7,4,5]},
+      {node,7,"green",[4,5,6]}]},
+     {"partition 2", [
+      {node,8,"red",[9,10,11,5]},
+      {node,9,"blue",[10,11,0,8]},
+      {node,10,"blue",[11,8,9]},
+      {node,11,"red",[8,9,10]}]}].
